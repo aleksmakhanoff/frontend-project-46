f173478a1f7c99db413bff7df2fc34f67394f2e9
aleksmakhanoff <152077911+aleksmakhanoff@users.noreply.github.com>
2024-07-19 16:58:39 +0700

    fix action

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import path from 'path';
+import fs from 'fs';
+import genDiff from '../src/index.js';
 
 const program = new Command();
 
@@ -9,5 +12,15 @@ program
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format')
   .arguments('<filepath1> <filepath2>')
-  .parse(process.argv);
-  
+  .action((filepath1, filepath2) => {
+    const absolutePath1 = path.resolve(filepath1);
+    const absolutePath2 = path.resolve(filepath2);
+
+    const data1 = JSON.parse(fs.readFileSync(absolutePath1, 'utf-8'));
+    const data2 = JSON.parse(fs.readFileSync(absolutePath2, 'utf-8'));
+
+    const diff = genDiff(data1, data2);
+    console.log(diff);
+  });
+
+program.parse(process.argv);
