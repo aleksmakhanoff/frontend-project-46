736013dc5c76b760f393bba9b7fd5cc840746ddf
aleksmakhanoff <152077911+aleksmakhanoff@users.noreply.github.com>
2024-07-27 21:36:55 +0700

    working for step 6

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,22 +4,26 @@ import { Command } from 'commander';
 import path from 'path';
 import fs from 'fs';
 import genDiff from '../src/index.js';
+import parse from '../src/parsers.js';
 
 const program = new Command();
 
 program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format [type]', 'output format')
+  .option('-f, --format [type]', 'output format', 'stylish') // Устанавливаем формат по умолчанию
   .arguments('<filepath1> <filepath2>')
-  .action((filepath1, filepath2) => {
+  .action((filepath1, filepath2, options) => {
     const absolutePath1 = path.resolve(filepath1);
     const absolutePath2 = path.resolve(filepath2);
 
-    const data1 = JSON.parse(fs.readFileSync(absolutePath1, 'utf-8'));
-    const data2 = JSON.parse(fs.readFileSync(absolutePath2, 'utf-8'));
+    const extname1 = path.extname(filepath1).slice(1);
+    const extname2 = path.extname(filepath2).slice(1);
 
-    const diff = genDiff(data1, data2);
+    const data1 = parse(fs.readFileSync(absolutePath1, 'utf-8'), extname1);
+    const data2 = parse(fs.readFileSync(absolutePath2, 'utf-8'), extname2);
+
+    const diff = genDiff(data1, data2, options.format);
     console.log(diff);
   });
 
--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 const genDiff = (data1, data2) => {
-    const allKeys = Object.keys({ ...data1, ...data2 }).sort();
-  
-    const result = allKeys.map((key) => {
-      if (data1[key] && !data2[key]) {
-        return `  - ${key}: ${data1[key]}`;
-      }
-      if (!data1[key] && data2[key]) {
-        return `  + ${key}: ${data2[key]}`;
-      }
-      if (data1[key] !== data2[key]) {
-        return `  - ${key}: ${data1[key]}\n  + ${key}: ${data2[key]}`;
-      }
-      return `    ${key}: ${data1[key]}`;
-    });
-  
-    return `{\n${result.join('\n')}\n}`;
-  };
-  
-  export default genDiff;
-  
+  const allKeys = Object.keys({ ...data1, ...data2 }).sort();
+
+  const result = allKeys.map((key) => {
+    if (data1[key] && !data2[key]) {
+      return `  - ${key}: ${data1[key]}`;
+    }
+    if (!data1[key] && data2[key]) {
+      return `  + ${key}: ${data2[key]}`;
+    }
+    if (data1[key] !== data2[key]) {
+      return `  - ${key}: ${data1[key]}\n  + ${key}: ${data2[key]}`;
+    }
+    return `    ${key}: ${data1[key]}`;
+  });
+
+  return `{\n${result.join('\n')}\n}`;
+};
+
+export default genDiff;
