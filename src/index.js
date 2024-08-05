eadea564175b76cba6db1eaf9443d036111e1ab8
aleksmakhanoff <152077911+aleksmakhanoff@users.noreply.github.com>
2024-08-05 17:18:54 +0700

    fix genDiff, add buildDiffTree

--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
+import stylish from './formatters/stylish.js';
+
 const genDiff = (data1, data2) => {
-  const allKeys = Object.keys({ ...data1, ...data2 }).sort();
+  const buildDiffTree = (data1, data2) => {
+    const allKeys = Object.keys({ ...data1, ...data2 }).sort();
 
-  const result = allKeys.map((key) => {
-    if (data1[key] && !data2[key]) {
-      return `  - ${key}: ${data1[key]}`;
-    }
-    if (!data1[key] && data2[key]) {
-      return `  + ${key}: ${data2[key]}`;
-    }
-    if (data1[key] !== data2[key]) {
-      return `  - ${key}: ${data1[key]}\n  + ${key}: ${data2[key]}`;
-    }
-    return `    ${key}: ${data1[key]}`;
-  });
+    return allKeys.map((key) => {
+      switch (true) {
+        case !Object.hasOwn(data2, key):
+          return { key, type: 'removed', value: data1[key] };
+        case !Object.hasOwn(data1, key):
+          return { key, type: 'added', value: data2[key] };
+        case typeof data1[key] === 'object' && typeof data2[key] === 'object':
+          return { key, type: 'nested', children: buildDiffTree(data1[key], data2[key]) };
+        case data1[key] !== data2[key]:
+          return { key, type: 'changed', oldValue: data1[key], newValue: data2[key] };
+        default:
+          return { key, type: 'unchanged', value: data1[key] };
+      }
+    });
+  };
 
-  return `{\n${result.join('\n')}\n}`;
+  const diffTree = buildDiffTree(data1, data2);
+  console.log(JSON.stringify(diffTree, null, 2));
+  return stylish(diffTree);
 };
 
 export default genDiff;
