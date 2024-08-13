dfe4e0ef2a5f6ec1efa5e552469ebf7a9b6c68cb
aleksmakhanoff <152077911+aleksmakhanoff@users.noreply.github.com>
2024-08-13 15:55:39 +0700

    added in tests all formatters

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,5 +1,6 @@
 import { test, expect } from '@jest/globals';
 import path from 'path';
+import fs from 'fs';
 import genDiff from '../src/index.js';
 
 const getFixturePath = (filename) => path.join('__fixtures__', filename);
@@ -9,57 +10,27 @@ const jsonFile2 = getFixturePath('file2.json');
 const yamlFile1 = getFixturePath('file1.yml');
 const yamlFile2 = getFixturePath('file2.yml');
 
-const expectedOutput = `{
-    common: {
-      + follow: false
-        setting1: Value 1
-      - setting2: 200
-      - setting3: true
-      + setting3: null
-      + setting4: blah blah
-      + setting5: {
-            key5: value5
-        }
-        setting6: {
-            doge: {
-              - wow: 
-              + wow: so much
-            }
-            key: value
-          + ops: vops
-        }
-    }
-    group1: {
-      - baz: bas
-      + baz: bars
-        foo: bar
-      - nest: {
-            key: value
-        }
-      + nest: str
-    }
-  - group2: {
-        abc: 12345
-        deep: {
-            id: 45
-        }
-    }
-  + group3: {
-        deep: {
-            id: {
-                number: 45
-            }
-        }
-        fee: 100500
-    }
-}`;
+const expectedStylishOutput = fs.readFileSync(getFixturePath('expectedStylish.txt'), 'utf-8');
+const expectedPlainOutput = fs.readFileSync(getFixturePath('expectedPlain.txt'), 'utf-8');
+const expectedJsonOutput = fs.readFileSync(getFixturePath('expectedJson.txt'), 'utf-8');
 
-test('gendiff with JSON files', () => {
-  const result = genDiff(jsonFile1, jsonFile2);
-  expect(result).toEqual(expectedOutput);
+test('gendiff with stylish format (JSON)', () => {
+  const result = genDiff(jsonFile1, jsonFile2, 'stylish');
+  console.log('Result:', result);
+  console.log('Expect:', expectedStylishOutput);
+  expect(result).toEqual(expectedStylishOutput);
 });
 
-test('gendiff with YAML files', () => {
-  const result = genDiff(yamlFile1, yamlFile2);
-  expect(result).toEqual(expectedOutput);
+test('gendiff with plain format (JSON)', () => {
+  const result = genDiff(jsonFile1, jsonFile2, 'plain');
+  console.log('Result:', result);
+  console.log('Expect:', expectedPlainOutput);
+  expect(result).toEqual(expectedPlainOutput);
+});
+
+test('gendiff with json format (JSON)', () => {
+  const result = genDiff(jsonFile1, jsonFile2, 'json');
+  console.log('Result:', result);
+  console.log('Expect:', expectedJsonOutput);
+  expect(result).toEqual(expectedJsonOutput);
 });
